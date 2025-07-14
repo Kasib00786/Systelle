b48c48852c32ded4d1cd1a78ce8f761e7743c21c
Kasib00786 <kasib7890@gmail.com>
2025-07-14 10:32:03 +0530

    daily update changes

--- frontend/src/component/DailyUpdate.jsx
+++ frontend/src/component/DailyUpdate.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import Menu from './Menu';
 const isactive='bg-gradient-to-r from-pink-100 to-indigo-200 pl-5 pr-5 rounded-full max-h-14 my-auto'
 const inactive='hover:bg-indigo-50 hover:shadow-lg rounded-full pr-5 pl-5 max-h-10 my-auto hover:scale-105 transition delay-100 duration-200 ease-in-out'
+const btn='rounded-xl bg-white  hover:scale-105  w-32';
 export const DailyUpdate = () => {
 const [open,setOpen]=useState(false);
   return (
@@ -24,24 +25,30 @@ const [open,setOpen]=useState(false);
                 )}
             </div>
         </div>
-        <div className='mt-16 max-w-[85%]  p-2 rounded-2xl mx-auto shadow-lg z-10 flex gap 2'>
-            <div className='bg-white/80 flex pl-2 max-w-[85%]'>
-                <h2 className='font-semibold text-lg'>1.How was your flowing?</h2>
-                <div className='flex gap-10'>
-                    <button>a</button>
-                    <button>b</button>
-                    <button>c</button>
-                    <button>d</button>
-                </div>
+        <div className='mt-16 max-w-[85%] bg-white/45 p-2 rounded-2xl mx-auto shadow-lg z-10 flex gap-2 justify-between'>
+          <div className='font-semibold text-lg pl-5'>1.How was your flowing?</div>
+            <div className='flex gap-2'>
+              <button className={btn}>Light</button>
+              <button className={btn}>Medium</button>
+              <button className={btn}>Heavy</button>
+              <button className={btn}>Super Heavy</button>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        </div>
+        <div className='mt-2 max-w-[85%] bg-white/50 p-2 rounded-2xl mx-auto shadow-lg z-10 flex gap-2 justify-between'>
+          <div className='font-semibold text-lg pl-5'>2.Any Spotting or irregular spotting?</div>
+            <div className='flex gap-2'>
+              <button className='rounded-xl bg-white outline outline-1 hover:scale-105  w-64'>Yes</button>
+              <button className='rounded-xl bg-white outline outline-1 hover:scale-105  w-64'>No</button>
+            </div>
+        </div>
+        <div className='mt-2 max-w-[85%] bg-white/50 p-2 rounded-2xl mx-auto shadow-lg z-10 flex gap-2 justify-between'>
+          <div className='font-semibold text-lg pl-5'>3.How are you felling?</div>
+            <div className='flex gap-2'>
+              <button className={btn}>Mood Swing</button>
+              <button className={btn}>Not in control</button>
+              <button className={btn}>Fine</button>
+              <button className={btn}>Happy</button>
+            </div>
         </div>
     </div>
   )
