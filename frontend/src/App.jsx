3e208752eea04ef9b67cd542d4a4a658e6b25805
KratiGupta04 <kratigupta0604@gmail.com>
2025-07-14 17:04:25 +0530

    added signup and updateform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,16 @@ import DailyUpdate from "./component/DailyUpdate";
 import Calendar from "./component/Calendar";
 import Login from "./component/Login";
 import Profile from "./component/Profile";
+import Signup from "./component/Signup";
+import Updateform from "./component/Updateform";
+
 
 const App=()=>{
   return <><BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />}/>
+      <Route path="/Signup" element={<Signup />}/>
+      <Route path="/Updateform" element={<Updateform />}/>
       <Route path="/home" element={<Navbar />}/>
       <Route path="/updates" element={<DailyUpdate />}/>
       <Route path="/calendar" element={<Calendar />}/>
---
+++ frontend/src/component/Signup.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Signup = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-[url(/base1.jpg)] bg-cover bg-center">
+            <div className="bg-white/50 backdrop-blur-lg outline outline-2 outline-white rounded-2xl p-10 w-full max-w-md shadow-xl">
+                <div className="flex mb-6">
+                    <img src="/logo.png" alt="logo" className='w-15 h-10' />
+                    <h1 className="text-2xl font-bold text-black">Systelle</h1>
+                </div>
+
+                <h1 className="text-center text-2xl font-bold text-black mb-6">
+                    Sign Up
+                </h1>
+
+                <form className="flex flex-col space-y-4">
+                    <input
+                        type="name"
+                        placeholder="Username"
+                        className="px-4 py-3 rounded-full border border-gray-400 focus:outline-none"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="px-4 py-3 rounded-full border border-gray-400 focus:outline-none"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="px-4 py-3 rounded-full border border-gray-400 focus:outline-none"
+                    />
+                    <Link to='./home' className="bg-violet-600 flex justify-center transition delay-100 duration-150 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-violet-700 text-white font-bold py-3 rounded-3xl"><button 
+                        type="submit"
+                    >
+                        Sign Up
+                    </button>
+                    </Link>
+                </form>
+
+                <div className="text-center mt-4 text-sm">
+                    <p className="mt-2 text-black">
+                        Have an account?{" "}
+                        <a href="#" className="text-purple-600 font-medium hover:underline">
+                            login
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
