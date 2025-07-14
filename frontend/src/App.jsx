f3fdc4c9ab5e7b1705fca3708f93438fe4443e2a
Kasib00786 <kasib7890@gmail.com>
2025-07-14 17:24:05 +0530

    signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App=()=>{
   return <><BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />}/>
-      <Route path="/Signup" element={<Signup />}/>
+      <Route path="/signup" element={<Signup />}/>
       <Route path="/form" element={<Updateform />}/>
       <Route path="/home" element={<Navbar />}/>
       <Route path="/updates" element={<DailyUpdate />}/>
