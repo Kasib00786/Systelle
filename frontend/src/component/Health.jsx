dcae52059852a417674fdc6d094b4d147260c8d8
Kasib00786 <kasib7890@gmail.com>
2025-07-15 23:38:08 +0530

    nothing

--- frontend/src/component/Health.jsx
+++ frontend/src/component/Health.jsx
@@ -10,33 +10,11 @@ const Health = () => {
 
   return (
     <div className="py-10 bg-[url(/base2.jpg)] bg-cover bg-center bg-fixed min-h-screen">
-<<<<<<< HEAD
-        <div className='flex justify-between  max-w-[85%] bg-white/80 p-2 rounded-2xl mx-auto shadow-lg z-10 flex-wrap'>
-            <div className='flex'>
-              <img src="/logo.png" alt="logo" className='w-20 h-12 my-auto' />
-            <div className='text-balance font-bold text-2xl my-auto mr-16'>
-                Systelle
-            </div>
-            </div>
-            <div className='text-lg font-bold flex flex-wrap gap-x-5'>
-                <Link to='/home' className={inactive}><button>Dashboard</button></Link>
-                <Link to='/calendar' className={inactive}><button>Calendar</button></Link>
-                <button className={isactive}>Health</button>
-                <Link to='/exercise' className={inactive}><button>Exercise</button></Link>
-                <button className='w-14 h-14 rounded-full bg-gray-300 shadow-sm ml-7 hover:scale-105 transition delay-100 duration-200 ease-in-out z-50' onClick={()=>setOpen(!open)}>👤</button>
-                {open && (
-                  <div className='fixed inset-0 bg-black/40 z-30 transition delay-150 duration-200 ease-in-out'>
-                    <Menu />
-                 </div>
-                )}
-            </div>
-=======
       {/* Navbar */}
       <div className='flex justify-between max-w-[85%] bg-white/80 p-2 rounded-2xl mx-auto shadow-lg z-10 flex-wrap'>
         <div className='flex'>
           <img src="/logo.png" alt="logo" className='w-20 h-12 my-auto' />
           <div className='text-balance font-bold text-2xl my-auto mr-16'>Systelle</div>
->>>>>>> 0a831fc2c33636412d0f56b97f6775fcd95e8dc6
         </div>
         <div className='text-lg font-bold flex flex-wrap gap-x-5'>
           <Link to='/home' className={inactive}><button>Dashboard</button></Link>
