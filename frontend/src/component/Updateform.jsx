c6ec6014a32cf1bf7a0432e188f41f8713112629
KratiGupta04 <kratigupta0604@gmail.com>
2025-07-17 16:24:12 +0530

    edited updateform

--- frontend/src/component/Updateform.jsx
+++ frontend/src/component/Updateform.jsx
@@ -105,6 +105,19 @@ export default function Updateform() {
               required
             />
           </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              • Your cycle last upto
+            </label>
+            <input
+              type="number"
+              name="cycleLength"
+              className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-400"
+              placeholder="e.g., 5"
+              required
+            />
+          </div>
+          
         </div>
 
         <input
